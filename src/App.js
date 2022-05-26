6e18632e8acde28f771ea276713c819d3e61b7fe
Shriram <shriramdeshpande77@gmail.com>
2022-05-26 22:14:11 +0530

    todo completed with json server and use effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
+import {v4 as uuidv4} from 'uuid';
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos,setTodos] = useState([])
+
+  const handleOnChange = (e) => {
+    setNewTodo(e.target.value)
+  };
+
+  const handleOnclick = (value) => {
+    fetch("http://localhost:8080/todos",{
+      method: "POST",
+      headers: {
+        "content-type":"application/json"
+      },
+      body: JSON.stringify({
+        "text": value,
+        "done": false
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      setTodos([...todos,data])
+      setNewTodo("")})
+  };
+  useEffect(()=>{
+    fetch("http://localhost:8080/todos")
+    .then(res => res.json())
+    .then(data => setTodos(data))
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo App Using API and useEffect</h1>
+      <div>
+        <input  type="text"
+                placeholder='Add New Todo'
+                onChange={handleOnChange}
+                value={newTodo}
+        />
+        <button onClick={()=>handleOnclick(newTodo)}>ADD</button>
+      </div>
+      <div>
+        {
+          todos.map((el)=>{
+            return <div key={el.id}>{el.text}</div>
+          })
+        }
+      </div>
     </div>
   );
 }
